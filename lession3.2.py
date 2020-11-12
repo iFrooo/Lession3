8a304be5765fcc75d07b31edc52176c977ea1206
iFrooo <73900995+iFrooo@users.noreply.github.com>
2020-11-12 17:21:51 +0300

    Add files via upload

---
+++ lession3.2.py
@@ -0,0 +1,18 @@
+'''
+Реализовать функцию, принимающую несколько параметров,
+описывающих данные пользователя: имя, фамилия, год рождения,
+город проживания, email, телефон. Функция должна принимать
+параметры как именованные аргументы. Реализовать вывод данных
+о пользователе одной строкой.
+'''
+'''
+    name = input('enter name')
+    surname = input('enter surname')
+    year = int(input('enter year'))
+    city = input('enter city')
+    email = input('enter email')
+    telephone = input('input telephone')
+'''
+def my_func (name, surname, year, city, email, telephone):
+     return ' '.join([name, surname, year, city, email, telephone])
+print(my_func(surname = 'Ivanov', name = 'Ivan', year = '1991', city = 'Tula', email = 'hide@mail.ru', telephone = '8-800-555-35-35'))
